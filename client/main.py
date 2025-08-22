e4938ec1000e34e0a68559794ad8f04d915cd480
David-Schmidt02 <daschmidt@frba.utn.edu.ar>
2025-08-21 21:25:12 -0300

    Añadir HEADLESS a la importación de configuración en main.py

--- client/main.py
+++ client/main.py
@@ -10,7 +10,7 @@ import random
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_dir)
 from my_logger import log
-from config import DEST_IP, DEST_PORT, METADATA_PORT, XVFB_DISPLAY, NUM_DISPLAY_PORT
+from config import DEST_IP, DEST_PORT, METADATA_PORT, XVFB_DISPLAY, HEADLESS, NUM_DISPLAY_PORT
 
 from client.audio_client_session import AudioClientSession
 from navigator_manager import Navigator
