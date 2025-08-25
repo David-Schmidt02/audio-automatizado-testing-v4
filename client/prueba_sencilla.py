9e8a059f7663b442e87db0787ecde1b74d9d65fe
David-Schmidt02 <davidadrianschmidt02@gmail.com>
2025-08-25 10:41:28 -0300

    Prueba pero ejecutando desde el interprete de python del env

--- client/prueba_sencilla.py
+++ client/prueba_sencilla.py
@@ -6,10 +6,11 @@ import subprocess
 def main():
     print("Hello, World!")
     script_path = os.path.abspath("prueba_sencilla.py") # convierte el main.py en una ruta absoluta
-    for a in range(2):
-        cmd = f"{sys.executable} {script_path}"
-        subprocess.Popen(["gnome-terminal", "--", "bash", "-c", cmd])
-        time.sleep(2)
+    interprete = os.path.expanduser("~/Desktop/Soflex/audio-test-env/bin/python")
+    python_env = os.path.expanduser("~/Desktop/Soflex/audio-test-env/bin/activate")
+    cmd = f"{interprete} {script_path}"
+    subprocess.Popen(["gnome-terminal", "--", "bash", "-c", cmd])
+    time.sleep(2)
 
 
 if __name__ == "__main__":
